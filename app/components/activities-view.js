8022b852b9a00a594d9dfadcea7f58c41b058b0e
jscottchapman <scott8675309@gmail.com>
2015-11-05 07:21:44 -0500

    beginning styling on activity page with added activity details

--- app/components/activities-view.js
+++ app/components/activities-view.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import store from '../store';
-import BackboneMixin from '../mixins/backbone'
+import BackboneMixin from '../mixins/backbone';
+
 const Activity = React.createClass({
   render() {
     var activity = this.props.activity;
     // console.log(this.props)
     return (
+
       <li>
         {activity.name}
+        <ul>
+          <li>
+            {activity.start_date_local}
+          </li>
+          <li>
+            {activity.distance}
+          </li>
+          <li>
+            {activity.type}
+          </li>
+        </ul>
         <select>
         {this.props.bikes.map((b)=> <option>{b.name}</option>)}
         </select>
@@ -36,12 +49,18 @@ const ActivitiesView = React.createClass({
   render(){
     var activities = this.state.activities;
     return(
-      <div>
+      <div className="row">
+        <div className="large-6 columns">
+        <div className="large-10 columns">
         <ul>
           {activities.map((x) =>
             <Activity activity={x} bikes={this.state.bikes} key={x.id} />
           )}
         </ul>
+        <div className="large-1 columns">
+        </div>
+        </div>
+        </div>
       </div>
     );
   }
